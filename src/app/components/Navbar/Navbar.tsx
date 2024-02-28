10020590fd0be8e67396185bde69c2562728f8b3
Elpah <obengelpachris@gmail.com>
2024-02-28 19:30:23 +0100

    navbar progess

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
-
+"use client";
+import React, { useState } from 'react'
+import { FaBars, FaTimes} from 'react-icons/fa';
 import styles from './navbar.module.scss';
+
 export default function Navbar() {
+ const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className={styles.container}>Navbar</div>
+    <header className={styles.container}>
+        <div className={styles.logo_container}>
+            <div className={styles.logo_container_image}>img</div>
+            <div className={styles.logo_container_name}>El-Pachris Obeng</div>
+        </div>
+        <div onClick={()=>setIsOpen(!isOpen)}>{isOpen? <FaTimes/>:<FaBars/>}</div>
+        {/* <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul> */}
+    </header>
+   
   )
 }
