73b4151352cbd8c72c1c69404945ccc73836e830
Elpah <obengelpachris@gmail.com>
2024-02-29 03:41:26 +0100

    save navbar progress

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,24 +1,45 @@
 "use client";
-import React, { useState } from 'react'
-import { FaBars, FaTimes} from 'react-icons/fa';
-import styles from './navbar.module.scss';
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import styles from "./navbar.module.scss";
+import Link from "next/link";
 
 export default function Navbar() {
- const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
   return (
-    <header className={styles.container}>
-        <div className={styles.logo_container}>
-            <div className={styles.logo_container_image}>img</div>
-            <div className={styles.logo_container_name}>El-Pachris Obeng</div>
-        </div>
-        <div onClick={()=>setIsOpen(!isOpen)}>{isOpen? <FaTimes/>:<FaBars/>}</div>
-        {/* <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Contact</li>
-        </ul> */}
-    </header>
-   
-  )
+    <nav className={styles.container}>
+      <div className={styles.logo_container}>
+        <img
+          className={styles.logo_container_img}
+          src="/images/pp.jpg"
+          alt="Image"
+        />
+        <div className={styles.logo_container_name}>El-Pachris Obeng</div>
+      </div>
+      {isOpen && (
+        <ul
+          className={styles.container_list}
+        >
+          <li className={styles.container_list_items}>
+            <Link href={"/"}>HOME</Link>
+          </li>
+          <li className={styles.container_list_items}>
+            <Link href={"/about"}>ABOUT</Link>
+          </li>
+          <li className={styles.container_list_items}>
+            <Link href={"/projects"}>PROJECTS</Link>
+          </li>
+          <li className={styles.container_list_items}>
+            <Link href={"/contact"}>CONTACT</Link>
+          </li>
+        </ul>
+      )}
+      <div
+        className={styles.container_hamburger}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </div>
+    </nav>
+  );
 }
