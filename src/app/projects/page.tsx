14b0a267a204b859ef9d9e05fc6b6260245eff1a
Elpah <obengelpachris@gmail.com>
2024-03-07 12:20:14 +0100

    merge project details and image into one card component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,56 +2,57 @@ import Image from "next/image";
 import styles from "./projects.module.scss";
 import Badge from "@/components/Badge/Badge";
 import ProjectCard from "@/components/ProjectCard/ProjectCard";
+
 export default function Projects() {
-  const Projects = [
+  const projects = [
     {
       projectName: "Portfolio",
-      projectAbout: "",
+      projectDetails: "My portfolio project that tells a bit about who i am and showcases some of my projects",
       toolsUsed: ["", "", "", "", ""],
     },
     {
       projectName: "Elpatronics",
-      projectAbout: "",
+      projectDetails: " A non commercial ecommerce website that demonstrates the online shopping process. Inluding features like signup, signin, paypal or visa checkout, find order and email confirmations.",
       toolsUsed: ["", "", "", "", ""],
     },
     {
       projectName: "Tinny Sitters",
-      projectAbout: "",
+      projectDetails: "An Application that allows users to book a baby sitter. ",
       toolsUsed: ["", "", "", "", ""],
     },
     {
       projectName: "Arith-magics",
-      projectAbout: "",
+      projectDetails: "A web application for math lovers that allows users to perform semi-complex and simple math operations, or read random math quotes ",
       toolsUsed: ["", "", "", "", ""],
     },
     {
       projectName: "JSArrayPlayGround",
-      projectAbout: "",
+      projectDetails: "An application that visualizes array operations.",
       toolsUsed: ["", "", "", "", ""],
     },
     {
       projectName: "Employee Portal",
-      projectAbout: "",
+      projectDetails: "A frontend application that performs displays a list of employers. It allows a user to create, read update and delete employees from the portal. ",
       toolsUsed: ["", "", "", "", ""],
     },
     {
       projectName: "Movie App",
-      projectAbout: "",
+      projectDetails: "A two day hackathon project project for fetching data from movie api, store in database and displays to user. allows users to also add likes to movie",
       toolsUsed: ["", "", "", "", ""],
     },
     {
       projectName: "FlickerFeed",
-      projectAbout: "",
+      projectDetails: "A web application that allows a user to search for data from the flicker public feed and displays it to the user.",
       toolsUsed: ["", "", "", "", ""],
     },
     {
       projectName: "Elpatronics",
-      projectAbout: "",
+      projectDetails: "",
       toolsUsed: ["", "", "", "", ""],
     },
     {
       projectName: "Elpatronics",
-      projectAbout: "",
+      projectDetails: "",
       toolsUsed: ["", "", "", "", ""],
     },
   ];
@@ -59,13 +60,9 @@ export default function Projects() {
     <main className={styles.main_container}>
       <Badge text="🚀Projects..." className={styles.badge} />
       <div className={styles.project_card_container}>
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {projects.map((project,index)=> <ProjectCard key={index} projectName={project.projectName} projectDetails={project.projectDetails}>
+          <img src="" alt="" />
+        </ProjectCard>)}
       </div>
     </main>
   );
--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,11 +1,61 @@
-import ProjectDetails from "./ProjectDetails";
-import ProjectImage from "./ProjectImage";
+import React from "react";
+import { ReactNode } from "react";
 import styles from "./project-card.module.scss";
-export default function ProjectCard() {
+
+
+type ProjectCardProps =
+{
+  projectName:string;
+  projectDetails:string;
+  children: ReactNode;
+
+}
+
+const renderChildrenWithClass = (children: React.ReactNode) => {
+  return React.Children.map(children, (child, index) => {
+    return React.cloneElement(child as React.ReactElement<any>, {
+      className: styles.tools_image,
+      key: index
+    });
+  });
+};
+
+const ToolsUsed = ({ children }: ProjectCardProps) => {
+  return (
+    <div className={styles.tools_used}>
+      {renderChildrenWithClass(children)}
+    </div>
+  );
+};
+
+
+export default function ProjectCard({projectName,projectDetails,children}:ProjectCardProps) {
   return (
     <div className={styles.card_container}>
-      <ProjectDetails/>
-      <ProjectImage/>
+    <div className={styles.details}>
+      <p className={styles.featured}>Featured Project</p>
+      <h2 className={styles.header}>{projectName}</h2>
+      <div className={styles.about_project}>
+        <p className={styles.about_project_text}>
+         {projectDetails}
+        </p>
+      </div>
+      <div className={styles.tools_used}>
+        {/* {children} */}
+       
+        {renderChildrenWithClass()}
+       
+       
+      </div>
     </div>
+     <div className={styles.image_container}>
+     <img
+       className={styles.project_image}
+       src="/images/project-images/imagetest.png"
+       alt="image"
+     />
+   </div>
+   </div>
+    
   );
 }
