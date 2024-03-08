cc03d23dece4cb42ad3205f3f5b8b812ed830ed2
Elpah <obengelpachris@gmail.com>
2024-03-08 01:07:50 +0100

    edited poject page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,62 +7,92 @@ export default function Projects() {
   const projects = [
     {
       projectName: "Portfolio",
-      projectDetails: "My portfolio project that tells a bit about who i am and showcases some of my projects",
-      toolsUsed: ["", "", "", "", ""],
+      projectDetails:
+        "My portfolio project that tells a bit about who i am and showcases some of my projects",
+      toolsUsed: [
+        "/images/skills_png/next.svg",
+        "/images/skills_png/framermotion.svg",
+        "/images/skills_png/sass.svg",     
+      ],
     },
     {
       projectName: "Elpatronics",
-      projectDetails: " A non commercial ecommerce website that demonstrates the online shopping process. Inluding features like signup, signin, paypal or visa checkout, find order and email confirmations.",
-      toolsUsed: ["", "", "", "", ""],
+      projectDetails:
+        " A non commercial ecommerce website that demonstrates the online shopping process. Inluding features like signup, signin, paypal or visa checkout, find order and email confirmations.",
+        toolsUsed: [
+          "/images/skills_png/react.svg",
+          "/images/skills_png/firebase.svg",
+          "/images/skills_png/mongodb.svg", 
+          "/images/skills_png/express.svg",  
+          "/images/skills_png/nodejs.svg",    
+        ],
     },
     {
       projectName: "Tinny Sitters",
-      projectDetails: "An Application that allows users to book a baby sitter. ",
-      toolsUsed: ["", "", "", "", ""],
-    },
+      projectDetails:
+        "An Application that allows users to book a baby sitter. ",
+        toolsUsed: [
+          "/images/skills_png/react.svg",
+          "/images/skills_png/firebase.svg",
+          "/images/skills_png/mongodb.svg", 
+          "/images/skills_png/express.svg",  
+          "/images/skills_png/nodejs.svg",  
+        ],    },
     {
       projectName: "Arith-magics",
-      projectDetails: "A web application for math lovers that allows users to perform semi-complex and simple math operations, or read random math quotes ",
-      toolsUsed: ["", "", "", "", ""],
+      projectDetails:
+        "A web application for math lovers that allows users to perform semi-complex and simple math operations, or read random math quotes ",
+      toolsUsed: [   "/images/skills_png/react.svg",
+     ],
     },
     {
       projectName: "JSArrayPlayGround",
       projectDetails: "An application that visualizes array operations.",
-      toolsUsed: ["", "", "", "", ""],
+      toolsUsed:          [ "/images/skills_png/react.svg",
+]
     },
     {
       projectName: "Employee Portal",
-      projectDetails: "A frontend application that performs displays a list of employers. It allows a user to create, read update and delete employees from the portal. ",
-      toolsUsed: ["", "", "", "", ""],
+      projectDetails:
+        "A frontend application that performs displays a list of employers. It allows a user to create, read update and delete employees from the portal. ",
+      toolsUsed: [ "/images/skills_png/react.svg"],
     },
     {
       projectName: "Movie App",
-      projectDetails: "A two day hackathon project project for fetching data from movie api, store in database and displays to user. allows users to also add likes to movie",
-      toolsUsed: ["", "", "", "", ""],
-    },
+      projectDetails:
+        "A two day hackathon project project for fetching data from movie api, store in database and displays to user. allows users to also add likes to movie",
+        toolsUsed: [
+          "/images/skills_png/react.svg",
+          "/images/skills_png/mongodb.svg", 
+          "/images/skills_png/express.svg",  
+          "/images/skills_png/nodejs.svg",    
+        ],    },
     {
       projectName: "FlickerFeed",
-      projectDetails: "A web application that allows a user to search for data from the flicker public feed and displays it to the user.",
-      toolsUsed: ["", "", "", "", ""],
-    },
-    {
-      projectName: "Elpatronics",
-      projectDetails: "",
-      toolsUsed: ["", "", "", "", ""],
-    },
-    {
-      projectName: "Elpatronics",
-      projectDetails: "",
-      toolsUsed: ["", "", "", "", ""],
-    },
+      projectDetails:
+        "A web application that allows a user to search for data from the flicker public feed and displays it to the user.",
+        toolsUsed: [
+          "/images/skills_png/next.svg",
+          "/images/skills_png/tailwind.svg",
+
+        ]},
+
   ];
   return (
     <main className={styles.main_container}>
       <Badge text="🚀Projects..." className={styles.badge} />
       <div className={styles.project_card_container}>
-        {projects.map((project,index)=> <ProjectCard key={index} projectName={project.projectName} projectDetails={project.projectDetails}>
-          <img src="" alt="" />
-        </ProjectCard>)}
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={index}
+            projectName={project.projectName}
+            projectDetails={project.projectDetails}
+          >
+            {project.toolsUsed.map((tool, index) => (
+              <img key={index} src={tool} alt="tool" />
+            ))}
+          </ProjectCard>
+        ))}
       </div>
     </main>
   );
--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -2,60 +2,49 @@ import React from "react";
 import { ReactNode } from "react";
 import styles from "./project-card.module.scss";
 
-
-type ProjectCardProps =
-{
-  projectName:string;
-  projectDetails:string;
+type ProjectCardProps = {
+  projectName: string;
+  projectDetails: string;
   children: ReactNode;
-
-}
-
-const renderChildrenWithClass = (children: React.ReactNode) => {
-  return React.Children.map(children, (child, index) => {
-    return React.cloneElement(child as React.ReactElement<any>, {
-      className: styles.tools_image,
-      key: index
-    });
-  });
 };
 
-const ToolsUsed = ({ children }: ProjectCardProps) => {
-  return (
-    <div className={styles.tools_used}>
-      {renderChildrenWithClass(children)}
-    </div>
+export default function ProjectCard({
+  projectName,
+  projectDetails,
+  children,
+}: ProjectCardProps) {
+
+  const imageChildren = React.Children.toArray(children).filter(
+    (child) => React.isValidElement(child) && child.type === 'img'
   );
-};
 
-
-export default function ProjectCard({projectName,projectDetails,children}:ProjectCardProps) {
   return (
     <div className={styles.card_container}>
-    <div className={styles.details}>
-      <p className={styles.featured}>Featured Project</p>
-      <h2 className={styles.header}>{projectName}</h2>
-      <div className={styles.about_project}>
-        <p className={styles.about_project_text}>
-         {projectDetails}
-        </p>
+      <div className={styles.details}>
+        <p className={styles.featured}>Featured Project</p>
+        <h2 className={styles.header}>{projectName}</h2>
+        <div className={styles.about_project}>
+          <p className={styles.about_project_text}>{projectDetails}</p>
+        </div>
+        <div className={styles.tools_used}>
+       {React.Children.map(children, (child, index) => {
+            if (React.isValidElement(child) && child.type === 'img') {
+              return React.cloneElement(child, {
+                className: `${child.props.className || ''} ${styles.tools_image}`,
+                key: index,
+              } as React.HTMLProps<HTMLImageElement>); 
+            }
+            return child;
+          })}
+        </div>
       </div>
-      <div className={styles.tools_used}>
-        {/* {children} */}
-       
-        {renderChildrenWithClass()}
-       
-       
+      <div className={styles.image_container}>
+        <img
+          className={styles.project_image}
+          src="/images/project-images/imagetest.png"
+          alt="image"
+        />
       </div>
     </div>
-     <div className={styles.image_container}>
-     <img
-       className={styles.project_image}
-       src="/images/project-images/imagetest.png"
-       alt="image"
-     />
-   </div>
-   </div>
-    
   );
 }
